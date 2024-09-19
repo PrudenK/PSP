da5de0cc18b6020c47fc9d213d4473d2112fd508
Pruden <prudencosta@gmail.com>
2024-09-19 12:16:40 +0200

    Ejercicios de repaso

---
+++ src/Ejs_1_Arrays_Bidimensionales/Ej_2.java
@@ -0,0 +1,24 @@
+package Ejs_1_Arrays_Bidimensionales;
+
+public class Ej_2 {
+
+    public static void main(String[] args) {
+        int[][] num = {
+                {0,30,2,0,0,5},
+                {75,0,0,0,0,0},
+                {0,0,-2,9,0,11},
+        };
+        System.out.printf("%14s","");
+        for (int i = 0; i < num[0].length; i++) {
+            System.out.printf("Column %-5d",i);
+        }
+        System.out.println();
+        for (int i = 0; i < num.length; i++) {
+            System.out.print("Fila "+i);
+            for (int j = 0; j < num[0].length; j++) {
+                System.out.printf("%12d",num[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Ejs_3_Excepciones/Ej_1.java
@@ -0,0 +1,27 @@
+package Ejs_3_Excepciones;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ej_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numIntro = 0;
+        int indiceNum = 1;
+        while (numIntro != -1){
+            System.out.println("\nPor favor, vaya introduciendo números enteros (-1 para salir).");
+            System.out.print("N "+indiceNum+": ");
+            try {
+                numIntro = sc.nextInt();
+                indiceNum++;
+            }catch (Exception e){
+                System.out.println("El dato introducido no es correcto, debe ser un número entero.");
+                System.out.println("Por favor, inténtelo de nuevo.");
+                sc.nextLine();
+            }
+        }
+        System.out.println("Programa terminado");
+        sc.close();
+    }
+}
